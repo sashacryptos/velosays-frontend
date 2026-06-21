b30396c1000fc6160f22eef8d9c6f9f01cbb801b
sashacryptos <sashachryptos@gmail.com>
2026-06-21 15:38:21 +0800

    update running coach dashboard UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx' // 👈 加上點字尾 .tsx 強迫路徑精準對齊
 import './index.css'
+import './App.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
